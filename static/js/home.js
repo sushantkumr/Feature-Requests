b08aef1b0aca99617e85ac5f4090682e509741f2
sushantkumr <10464008+sushantkumr@users.noreply.github.com>
2019-01-14 17:02:35 +0530

    Basic drag and drop implemented

--- static/js/home.js
+++ static/js/home.js
@@ -9,11 +9,24 @@ $(document).ready(function() {
                     ec.utils.errorHandler(response);
                     return;
                 }
+                console.log(response.data)
                 self.featureRequests(response.data);
             });
         }
         self.getFeatureRequests();
     };
+
+    //control visibility, give element focus, and select the contents (in order)
+    ko.bindingHandlers.visibleAndSelect = {
+        update: function(element, valueAccessor) {
+            ko.bindingHandlers.visible.update(element, valueAccessor);
+            if (valueAccessor()) {
+                setTimeout(function() {
+                    $(element).find("input").focus().select();
+                }, 0); //new tasks are not in DOM yet
+            }
+        }
+    };    
 
     _viewModel = new FeatureRequestsViewModel();
     ko.applyBindings(_viewModel);
