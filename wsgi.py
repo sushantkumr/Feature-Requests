764c0a6160a6efe5fa0215f8f03ba745a744e0d5
sushantkumr <10464008+sushantkumr@users.noreply.github.com>
2019-01-17 15:14:24 +0530

    Adding wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+# Flaky AF
+# https://www.digitalocean.com/community/questions/no-python-application-found-uwsgi-flask-nginx
+
+from server import app as application
+
+if __name__ == '__main__':
+    application.run()
